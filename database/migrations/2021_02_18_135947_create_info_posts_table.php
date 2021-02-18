4f9087e8500bab63704856fd776df81edf79333c
Stefano Franchini <stefano.franchini77@gmail.com>
2021-02-18 15:11:21 +0100

    create le 3 tabelle ed i rispettivi model

---
+++ database/migrations/2021_02_18_135947_create_info_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInfoPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('info_posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->string('post_status', 7);  // private | public | draft
+            $table->string('comment_status', 7); // open | closed | private
+            $table->timestamps();
+
+            $table->foreign('post_id')
+                ->references('id')
+                ->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('info_posts');
+    }
+}
